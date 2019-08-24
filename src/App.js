622dbc0e0473762bd0d9e5be67cee2099e214661
Stuart Elimu <41069456+ElimuStuart@users.noreply.github.com>
2019-08-24 09:37:47 +0300

    Boiler plate
    created simple react boiler plate that shows something

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
