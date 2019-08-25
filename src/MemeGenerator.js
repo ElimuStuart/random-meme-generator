3fb10649125a250938d713aa3a8cf0634c7e1bc2
Stuart Elimu <41069456+ElimuStuart@users.noreply.github.com>
2019-08-25 06:51:00 +0300

    Completed
    MemeGenerator completed but having CORS error

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -3,10 +3,61 @@ import React, { Component } from "react";
 export default class MemeGenerator extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      topText: "",
+      bottomText: "",
+      randomImg: "https://i.imgflip.com/38sbs4.jpg",
+      allMemeImgs: []
+    };
   }
 
+  componentDidMount() {
+    fetch("https://api.imgflip.com/get_memes")
+      .then(response => response.json())
+      .then(response => {
+        const { memes } = response.data;
+        this.setState({ allMemeImgs: memes });
+      })
+      .catch(e => console.log("error", e));
+  }
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const randNum = Math.floor(Math.random() * this.state.allMemeImgs.length);
+    const randMemeImg = this.state.randomImg[randNum].url;
+    this.setState({ randomImg: randMemeImg });
+  };
+
   render() {
-    return <h2>Start editing to see some magic happen!</h2>;
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            name="topText"
+            type="text"
+            value={this.state.topText}
+            onChange={this.handleChange}
+          />
+          <input
+            name="bottomText"
+            type="text"
+            value={this.state.bottomText}
+            onChange={this.handleChange}
+          />
+          <button>Gen</button>
+        </form>
+
+        <div className="meme">
+          <img src={this.state.randomImg} alt="" />
+          <h2 className="top">{this.state.topText}</h2>
+          <h2 className="bottom">{this.state.bottomText}</h2>
+        </div>
+      </div>
+    );
   }
 }
